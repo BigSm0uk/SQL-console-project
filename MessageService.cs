92dd754052fb620b08cb1b213bfc78b29ac3b4f1
BigSm0uk <88782528+BigSm0uk@users.noreply.github.com>
2022-04-20 15:25:03 +0300

    Add files via upload

---
+++ MessageService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_sqlTool_projects
+{
+    interface IMessageService
+    {
+        void ShowMessage(string message);
+    }
+    public class MessageService : IMessageService
+    {
+        public void ShowMessage(string message)
+        {
+            Console.WriteLine($"Комманда {message} некорректна!");
+        }
+    }
+}
