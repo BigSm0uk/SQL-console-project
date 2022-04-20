cd27a4da833d516af97749f1caf334b5adeea33b
BigSm0uk <88782528+BigSm0uk@users.noreply.github.com>
2022-04-20 11:24:46 +0300

    Update StudentAPP.cs
    Добавил сортировку и поиск

--- StudentAPP.cs
+++ StudentAPP.cs
@@ -10,6 +10,9 @@ using System.Configuration;
 #region User Manual
 /* 
     1. Выход - exit
+    2. Выборка - Select
+    3.Изменение - Update
+    4.Удаление - Delete
 
  */
 #endregion
@@ -26,35 +29,39 @@ namespace StudentAPP
             connection.Open();
             Console.WriteLine("Hello");
             SqlDataReader dataReader = null;
-            string command = string.Empty;
-
-            while(true)
-            {
-                Console.Write("> ");
-                command = Console.ReadLine();
-
-                #region Exit
-                if (command.ToLower().Trim() == "exit")
-                {
-                    if(connection.State != ConnectionState.Open)
-                    {
-                        connection.Close();
-                    }
-                    if (dataReader != null)
-                    {
-                        dataReader.Close();
-                    }
-                    break;
-                }
-                #endregion
+            string command = string.Empty; 
+            
+            while (true)
+            {               
                 try
                 {
-                    SqlCommand sql = new SqlCommand(command, connection);
+                    Console.Write("> ");
+                    command = Console.ReadLine();
 
-                    switch (command.Split(' ')[0].ToLower())
+                    #region Exit
+                    if (command.ToLower().Trim() == "exit")
+                    {
+                        if (connection.State != ConnectionState.Open)
+                        {
+                            connection.Close();
+                        }
+                        if (dataReader != null)
+                        {
+                            dataReader.Close();
+                        }
+                        break;
+                    }
+                    #endregion
+                    SqlCommand sqlcommand = null;
+                    string [] command_array = command.ToLower().Split(' ');
+
+                    switch (command_array[0])
                     {
                         case "select":
-                            dataReader = sql.ExecuteReader();
+
+                            sqlcommand = new SqlCommand(command, connection);
+
+                            dataReader = sqlcommand.ExecuteReader();
                             while (dataReader.Read())
                             {
                                 Console.WriteLine($"{dataReader["Id"]} {dataReader["FIO"]} {dataReader["Birthday"]} {dataReader["University"]} " +
@@ -68,23 +75,71 @@ namespace StudentAPP
 
                             break;
                         case "insert":
-
-                            Console.WriteLine($"Добавлено: {sql.ExecuteNonQuery()} строк(а)");
-
-
+                             sqlcommand = new SqlCommand(command, connection);
+                            Console.WriteLine($"Добавлено: {sqlcommand.ExecuteNonQuery()} строк(а)");
                             break;
                         case "update":
-
-                            Console.WriteLine($"Изменено: {sql.ExecuteNonQuery()} строк(а)");
-
+                            sqlcommand = new SqlCommand(command, connection);
+                            Console.WriteLine($"Изменено: {sqlcommand.ExecuteNonQuery()} строк(а)");
                             break;
-
-
-
                         case "delete":
-
-                            Console.WriteLine($"Удалено: {sql.ExecuteNonQuery()} строк(а)");
+                             sqlcommand = new SqlCommand(command, connection);
+                            Console.WriteLine($"Удалено: {sqlcommand.ExecuteNonQuery()} строк(а)");
                             break;
+                        case "sortby":
+                            sqlcommand = new SqlCommand($"Select * from students order by {command_array[1]} {command_array[2]}", connection);
+                            dataReader = sqlcommand.ExecuteReader();
+                            while (dataReader.Read())
+                            {
+                                Console.WriteLine($"{dataReader["Id"]} {dataReader["FIO"]} {dataReader["Birthday"]} {dataReader["University"]} " +
+                                    $"{dataReader["Group_number"]} {dataReader["Course"]} {dataReader["AVG_score"]}");
+                                Console.WriteLine(new string('-', 60));
+                            }
+                            if (dataReader != null)
+                            {
+                                dataReader.Close();
+                            }
+                            break;
+
+                        case "search":
+                            if (command_array[1].Equals("fio"))
+                            {
+                                sqlcommand = new SqlCommand($"Select * from Students where fio like N'%{command_array[2]}%'" , connection);
+                            }
+                            else if (command_array[1].Equals("birthday"))
+                            {
+                                sqlcommand = new SqlCommand($"Select * from Students where birthday = '{command_array[2]}'", connection);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Аргумент {command} не корректен!");
+                            }
+                            try
+                            {
+                                dataReader = sqlcommand.ExecuteReader();
+                                while (dataReader.Read())
+                                {
+                                    Console.WriteLine($"{dataReader["Id"]} {dataReader["FIO"]} {dataReader["Birthday"]} {dataReader["University"]} " +
+                                        $"{dataReader["Group_number"]} {dataReader["Course"]} {dataReader["AVG_score"]}");
+                                    Console.WriteLine(new string('-', 60));
+                                }
+                                if (dataReader != null)
+                                {
+                                    dataReader.Close();
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                            finally
+                            {
+                                if (dataReader != null)
+                                {
+                                    dataReader.Close();
+                                }
+                            }
+                                break;
                         default:
                             Console.WriteLine($"Комманда {command} некорректна!");
                             break;
